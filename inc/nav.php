bab68a8a14fc3467fb53be0ca9e387a4f4b2b962
mmanchen <mmanchen@u.rochester.edu>
2019-11-13 21:13:55 -0500

    Create nav.php

---
+++ inc/nav.php
@@ -0,0 +1,8 @@
+	<nav class="menu">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="practice.php">Practice</a></li>
+			<li><a href="contact-us.php">Contact Us</a></li>
+		</ul>
+	</nav>
