fa2bd09a05381bdd0490119624a751c4a200698a
mmanchen <mmanchen@u.rochester.edu>
2019-11-13 21:13:37 -0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Women's Soccer Club | Lab 6 </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+<?php include "inc/nav.php"; ?>
+
+	<main>
+		<h1> About </h1>
+
+		<p> The women's soccer club at the University of Rochester became an official club in 2005. There were only 11 members in the team and event though they were not many, they were enough to form a soccer team. The soccer team doesn't have a coach, so every year the roster chooses one the team members as the new captain. The captain is in charge of organizing the team, games, practice plans and deciding upon the team's roster. This past season, the team had the opportunity to face Buffalo, Niagara, Brockport, St. Bonaventure, Canisius, RIT, Fredonia and Oswego. The club is currently ran by Maddy Candela.   </p>
+        <img src= "images/game.png" alt="game">
+
+	</main>
+
+    <img src= "images/old-roster.png" alt="old roster">
+    <aside>
+		<ul>
+            <h1> Roster </h1>
+			<li>Alison Ciurzynski</li>
+            <li>Chloe Jones</li>
+            <li>Claire Cao</li>
+            <li>Delaney Martino</li>
+            <li>Divya Duraisamy</li>
+            <li>Emily Kumpf</li>
+            <li>Gianna Cicileo</li>
+            <li>Jessica Sgambati</li>
+            <li>Jessica Wood</li>
+            <li>Katie Simons</li>
+            <li>Kirsten Brownell</li>
+            <li>Kyra Majewski</li>
+            <li>Maddi Lang</li>
+            <li>Madelyn Candela</li>
+            <li>Maia Warner</li>
+            <li>Maria Mancheno</li>
+            <li>Michaela Farmer</li>
+            <li>Mimi Jung</li>
+            <li>Nipu Berger</li>
+            <li>Rebecca Friedman</li>
+            <li>Reena Caplash</li>
+            <li>Rochelle Vassell</li>
+            
+		</ul>
+	</aside>
+
+	
+	<footer> <strong> Website developed by Team Berlin </strong></footer>
+	
+
+</body></html>
