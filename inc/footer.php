df1ef2267b720bc4e62543d4e9dbf8c0efafe16d
Islomzhan Akhmedov <islomzhan@gmail.com>
2019-11-17 18:35:18 -0500

    updated footer for responsive page style

--- inc/footer.php
+++ inc/footer.php
@@ -1,36 +1,28 @@
 
     <footer>
       <!--- Connect -->
-
       <div class="container-fluid padding">
         <hr class= "light">
         <div class="row text-center padding">
-            <div class="col-xs-12 col-sm-6 col-md-3 social-title">
+            <div class="col-lg-3 social-title">
                 <h2>Connect Us:</h2>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-6 social padding">
-
+            <div class="col-lg-6 social padding">
                 <a href="http://www.facebook.com/UrWomensClubSoccer"><i class="fa fa-facebook"></i></a>
-
                 <a href="https://twitter.com/URWCSoccer"><i class="fa fa-twitter"></i></a>
-
                 <a href="https://www.instagram.com/urwcsoccer"><i class="fa fa-instagram"></i></a>
             </div>
-            <div class="col-sm-6 col-md-3 social-tag">
+            <div class="col-lg-3 social-tag">
                 <p class="display-4">#URWSC</p>
             </div>
             <hr class= "light">
         </div>
-
         <div class="row text-center">
           <div class="col-12 footer-notice">
             <h5> Website developed by Team Berlin </h5>
           </div>
-
         </div>
-
       </div>
-
      </footer>
     <!-- Optional JavaScript -->
     <script src="https://use.fontawesome.com/9bd6d54fab.js"></script>
