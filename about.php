b7b127bdb79a03398eb1d57b81b1446c626cd975
Islomzhan Akhmedov <islomzhan@gmail.com>
2019-11-17 18:31:24 -0500

    about page's ground ready design

--- about.php
+++ about.php
@@ -7,34 +7,41 @@ include "inc/head+nav.php";
 	   <h1> About </h1>
 	   <p> The women's soccer club at the University of Rochester became an official club in 2005. There were only 11 members in the team and event though they were not many, they were enough to form a soccer team. The soccer team doesn't have a coach, so every year the roster chooses one the team members as the new captain. The captain is in charge of organizing the team, games, practice plans and deciding upon the team's roster. This past season, the team had the opportunity to face Buffalo, Niagara, Brockport, St. Bonaventure, Canisius, RIT, Fredonia and Oswego. The club is currently ran by Maddy Candela.   </p>
 	</main>
-	<img class = "about-image" src= "images/game.png" alt="game">
-	<img  class = "about-image" src= "images/old-roster.png" alt="old roster">
-	<aside>
-	   <ul>
-	      <h1> Roster </h1>
-	      <li>Alison Ciurzynski</li>
-	      <li>Chloe Jones</li>
-	      <li>Claire Cao</li>
-	      <li>Delaney Martino</li>
-	      <li>Divya Duraisamy</li>
-	      <li>Emily Kumpf</li>
-	      <li>Gianna Cicileo</li>
-	      <li>Jessica Sgambati</li>
-	      <li>Jessica Wood</li>
-	      <li>Katie Simons</li>
-	      <li>Kirsten Brownell</li>
-	      <li>Kyra Majewski</li>
-	      <li>Maddi Lang</li>
-	      <li>Madelyn Candela</li>
-	      <li>Maia Warner</li>
-	      <li>Maria Mancheno</li>
-	      <li>Michaela Farmer</li>
-	      <li>Mimi Jung</li>
-	      <li>Nipu Berger</li>
-	      <li>Rebecca Friedman</li>
-	      <li>Reena Caplash</li>
-	      <li>Rochelle Vassell</li>
-	   </ul>
+	<aside class = "container-fluid p-5">
+		<hr>
+		<h1 class="ml-4"> Roster </h1>
+		<div class="row content">
+				<div class = "col-lg-5">
+					<ol class = "roster-list">
+		 	      <li>Alison Ciurzynski</li>
+		 	      <li>Chloe Jones</li>
+		 	      <li>Claire Cao</li>
+		 	      <li>Delaney Martino</li>
+		 	      <li>Divya Duraisamy</li>
+		 	      <li>Emily Kumpf</li>
+		 	      <li>Gianna Cicileo</li>
+		 	      <li>Jessica Sgambati</li>
+		 	      <li>Jessica Wood</li>
+		 	      <li>Katie Simons</li>
+		 	      <li>Kirsten Brownell</li>
+		 	      <li>Kyra Majewski</li>
+		 	      <li>Maddi Lang</li>
+		 	      <li>Madelyn Candela</li>
+		 	      <li>Maia Warner</li>
+		 	      <li>Maria Mancheno</li>
+		 	      <li>Michaela Farmer</li>
+		 	      <li>Mimi Jung</li>
+		 	      <li>Nipu Berger</li>
+		 	      <li>Rebecca Friedman</li>
+		 	      <li>Reena Caplash</li>
+		 	      <li>Rochelle Vassell</li>
+			 	   </ol>
+					</div>
+					<div class = "col-lg-7 text-center">
+						<img class = "about-image" src= "images/game.png" alt="game">
+						<img  class = "about-image" src= "images/old-roster.png" alt="old roster">
+					</div>
+		</div>
 	</aside>
 
 <?php include "inc/footer.php"; ?>
